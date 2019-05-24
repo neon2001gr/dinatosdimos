01a78503e9b6edf06f0583a5d82cee08c30e747a
AlexTheGreek <36368139+neon2001gr@users.noreply.github.com>
2019-05-24 16:12:38 +0300

    as
    as

--- js/demo/files.js
+++ js/demo/files.js
@@ -4,11 +4,29 @@ var enswmatwsi = 10;
 var pososta = [10, 33, 44, 55, 77, 55,99] 
 var pososta_ana_eklogiko = [42, 53, 62, 500, 98, 14];
 
-var dinatosdimos_apotelesmata_dimotika	= [	{ y: 243, label: "1ο Σαλαμίνας" },
-											{ y: 236, label: "2ο Σαλαμίνας" },
-											{ y: 243, label: "Αιαντείου" },
-											{ y: 273, label: "Αμπελακίων" },
-											{ y: 269, label: "Σελήνια" }];
+dynatos = { 
+			'dimotika':{
+					'sal_1o':200, 
+					'sal_2o':200, 
+					'aianteio':200, 
+					'ampelakia':200, 
+					'selinia':200, 
+					  }
+			'koinwtika':{
+					'sal_1o':200, 
+					'sal_2o':200, 
+					'aianteio':200, 
+					'ampelakia':200, 
+					'selinia':200, 
+					  }
+					  
+		  }
+
+var dinatosdimos_apotelesmata_dimotika	= [	{ y: dynatos['dimotika']['sal_1o'], label: "1ο Σαλαμίνας" },
+											{ y: dynatos['dimotika']['sal_2o'], label: "2ο Σαλαμίνας" },
+											{ y: dynatos['dimotika']['aianteio'], label: "Αιαντείου" },
+											{ y: dynatos['dimotika']['ampelakia'], label: "Αμπελακίων" },
+											{ y: dynatos['dimotika']['selinia'], label: "Σελήνια" }];
 
 var nisi_apotelesmata_dimotika 			= [	{ y: 243, label: "1ο Σαλαμίνας" },
 											{ y: 236, label: "2ο Σαλαμίνας" },
@@ -40,6 +58,9 @@ var laiki_apotelesmata_dimotika 		= [	{ y: 243, label: "1ο Σαλαμίνας" 
 											{ y: 243, label: "Αιαντείου" },
 											{ y: 273, label: "Αμπελακίων" },
 											{ y: 269, label: "Σελήνια" }];
+											
+											
+											
 var dinatosdimos_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
 											{ y: 236, label: "2ο Σαλαμίνας" },
 											{ y: 243, label: "Αιαντείου" },
