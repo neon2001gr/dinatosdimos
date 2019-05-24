59e3956107c357541fdabad064e1ff44fe7b0e94
AlexTheGreek <36368139+neon2001gr@users.noreply.github.com>
2019-05-24 11:13:23 +0300

    Update files.js

--- js/demo/files.js
+++ js/demo/files.js
@@ -4,13 +4,79 @@ var enswmatwsi = 10;
 var pososta = [10, 33, 44, 55, 77, 55,99] 
 var pososta_ana_eklogiko = [42, 53, 62, 500, 98, 14];
 
-var dinatosdimos_apotelesmata = [{ y: 243, label: "1ο Σαλαμίνας" },
-									{ y: 236, label: "2ο Σαλαμίνας" },
-									{ y: 243, label: "Αιαντείου" },
-									{ y: 273, label: "Αμπελακίων" },
-									{ y: 269, label: "Σελήνια" }]
+var dinatosdimos_apotelesmata_dimotika	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
 
+var nisi_apotelesmata_dimotika 			= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var konta_apotelesmata_dimotika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var protovoulia_apotelesmata_dimotika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var anasygkrotisi_apotelesmata_dimotika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var vima_apotelesmata_dimotika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var laiki_apotelesmata_dimotika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var dinatosdimos_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var nisi_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var protovoulia_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var anasygkrotisi_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var vima_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var laiki_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
+var travlos_apotelesmata_topika 	= [	{ y: 243, label: "1ο Σαλαμίνας" },
+											{ y: 236, label: "2ο Σαλαμίνας" },
+											{ y: 243, label: "Αιαντείου" },
+											{ y: 273, label: "Αμπελακίων" },
+											{ y: 269, label: "Σελήνια" }]
 
+											
 document.getElementById("dinatos_dimos_pososto").innerHTML 	 = String(pososta[0])+'%';
 document.getElementById("nisi_pososto").innerHTML       	 = String(pososta[1])+'%';
 document.getElementById("konta_dimoti_pososto").innerHTML 	 = String(pososta[2])+'%';
