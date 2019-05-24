a3f47be273d8ede93299e1624e62beff0c30dac0
AlexTheGreek <36368139+neon2001gr@users.noreply.github.com>
2019-05-24 17:15:25 +0300

    asd

--- js/demo/files.js
+++ js/demo/files.js
@@ -1,4 +1,4 @@
-var date_time  = "17:11:17";
+var date_time  = "17:15:07";
 var enswmatwsi =  0;
 var dynatos = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'dimotika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'synoliko': 0.0};
 var nisi = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'dimotika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'synoliko': 0.0};
@@ -7,3 +7,5 @@ var protovoulia = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selin
 var anasygkrotisi = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'dimotika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'synoliko': 0.0};
 var vima = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'dimotika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'synoliko': 0.0};
 var travlos = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}};
+var laiki = {'koinwtika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'dimotika': {'sal_2o': 0, 'sal_1o': 0, 'aianteio': 0, 'selinia': 0, 'ampelakia': 0}, 'synoliko': 0.0};
+
