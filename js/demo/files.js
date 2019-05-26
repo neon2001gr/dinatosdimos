2747a62288340c073f84a6fbf5a45f48fbb90ea8
AlexTheGreek <36368139+neon2001gr@users.noreply.github.com>
2019-05-26 23:13:35 +0300

    Update files.js

--- js/demo/files.js
+++ js/demo/files.js
@@ -1,4 +1,4 @@
-var date_time  = "23:12:15";
+var date_time  = "23:13:17";
 var enswmatwsi =  36.0;
 var dynatos = {'koinwtika': {'sal_2o': 0.0, 'sal_1o': 100.0, 'aianteio': 0.0, 'selinia': 0.0, 'ampelakia': 0.0}, 'dimotika': {'sal_2o': 17.13, 'sal_1o': 19.58, 'aianteio': 12.0, 'selinia': 0.0, 'ampelakia': 12.77}, 'synoliko': 16.65};
 var nisi = {'koinwtika': {'sal_2o': 0.0, 'sal_1o': 0.0, 'aianteio': 0.0, 'selinia': 0.0, 'ampelakia': 0.0}, 'dimotika': {'sal_2o': 33.96, 'sal_1o': 28.91, 'aianteio': 25.86, 'selinia': 0.0, 'ampelakia': 29.92}, 'synoliko': 30.86};
