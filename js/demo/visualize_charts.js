8554f6ae8ecdccb044da2c0253faaa6941bfa916
AlexTheGreek <36368139+neon2001gr@users.noreply.github.com>
2019-05-26 21:40:11 +0300

    as

--- js/demo/visualize_charts.js
+++ js/demo/visualize_charts.js
@@ -1,2 +1,2 @@
 document.getElementById("date_time_charts").innerHTML 		 = "Τελευταία Ενημέρωση: Κυριακή 26.05.2019, " + date_time ;
-document.getElementById("enswmatwsi_charts").innerHTML 		 = "Ενσωμάτωση: " + enswmatwsi + "/62";
+document.getElementById("enswmatwsi_charts").innerHTML 		 = "Ενσωμάτωση: " + enswmatwsi + "/62"; 
